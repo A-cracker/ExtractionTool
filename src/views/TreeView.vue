9afc8cd2caf27e4db521cdf0aac21c9582ccb5c1
lznkszsdx <1281590115@qq.com>
2023-08-09 15:43:57 +0800

    增加滑动条

--- src/views/TreeView.vue
+++ src/views/TreeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tree-view">
-    <div class="tree">
+  <div class="tree-view box">
+    <div class="tree left">
       <div class="tree-wrap" style="height: 100%;">
         <div class="search-box">
           <span>搜索：</span>
@@ -49,8 +49,9 @@
         </div>
       </div>
     </div>
+    <div class="resize">⋮</div>
     <!--分析点内容-->
-    <div class="detail">
+    <div class="detail mid">
       <div class="search">
         <el-input
             v-model="searchWord"
@@ -290,8 +291,53 @@ export default {
   },
   created() {
     this.getInitTreeData()
+
+  },
+  mounted() {
+    this.dragControllerDiv();
   },
   methods: {
+    dragControllerDiv: function() {
+      const resize = document.getElementsByClassName('resize');
+      const left = document.getElementsByClassName('left');
+      const mid = document.getElementsByClassName('mid');
+      const box = document.getElementsByClassName('box');
+      for (let i = 0; i < resize.length; i++) {
+        // 鼠标按下事件
+        resize[i].onmousedown = function (e) {
+          //颜色改变提醒
+          resize[i].style.background = '#818181';
+          const startX = e.clientX;
+          resize[i].left = resize[i].offsetLeft;
+          // 鼠标拖动事件
+          document.onmousemove = function (e) {
+            const endX = e.clientX;
+            let moveLen = resize[i].left + (endX - startX); // （endx-startx）=移动的距离。resize[i].left+移动的距离=左边区域最后的宽度
+            const maxT = box[i].clientWidth - resize[i].offsetWidth; // 容器宽度 - 左边区域的宽度 = 右边区域的宽度
+
+            moveLen = moveLen < 32 ? 32 : moveLen; // 左边区域的最小宽度为32px
+            moveLen = moveLen > (maxT - 150) ? maxT - 150 : moveLen; //右边区域最小宽度为150px
+
+            resize[i].style.left = moveLen; // 设置左侧区域的宽度
+
+            for (let j = 0; j < left.length; j++) {
+              left[j].style.width = moveLen + 'px';
+              mid[j].style.width = (box[i].clientWidth - moveLen - 10) + 'px';
+            }
+          };
+          // 鼠标松开事件
+          document.onmouseup = function (evt) {
+            //颜色恢复
+            resize[i].style.background = '#d6d6d6';
+            document.onmousemove = null;
+            document.onmouseup = null;
+            resize[i].releaseCapture && resize[i].releaseCapture(); //当你不在需要继续获得鼠标消息就要应该调用ReleaseCapture()释放掉
+          };
+          resize[i].setCapture && resize[i].setCapture(); //该函数在属于当前线程的指定窗口里设置鼠标捕获
+          return false;
+        };
+      }
+    },
     async addNodeClick() {
       this.dialogFormVisible = false
       console.log(this.addId)
@@ -445,15 +491,50 @@ export default {
   }
 }
 @media(min-width: 768px) {
+  .left {
+    width: calc(32% - 10px);  /*左侧初始化宽度*/
+    height: 100%;
+    background: #FFFFFF;
+    float: left;
+  }
+  /*拖拽区div样式*/
+  .resize {
+    cursor: col-resize;
+    float: left;
+    position: relative;
+    top: 45%;
+    background-color: #d6d6d6;
+    border-radius: 5px;
+    margin-top: -10px;
+    width: 8px;
+    height: 50px;
+    background-size: cover;
+    background-position: center;
+    /*z-index: 99999;*/
+    font-size: 28px;
+    color: white;
+  }
+  /*拖拽区鼠标悬停样式*/
+  .resize:hover {
+    color: #444444;
+  }
+  /*右侧div'样式*/
+  .mid {
+    float: left;
+    width: 68%;   /*右侧初始化宽度*/
+    height: 100%;
+    background: #fff;
+    box-shadow: -1px 4px 5px 3px rgba(0, 0, 0, 0.11);
+  }
   .tree-view {
     height: 100%;
     display: flex;
     .tree {
-      width: 60%;
+      min-width: 30%;
       height: 100%;
     }
     .detail {
-      width:40%;
+      min-width: 30%;
       height: 100%;
     }
   }
@@ -468,6 +549,9 @@ export default {
     }
     .detail {
       height: 50%;
+    }
+    .resize{
+      display: none;
     }
   }
 }
